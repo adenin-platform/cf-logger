e39e36736642c75f0a4da59e856fb21074e99d5d
olliejm <olliejm@protonmail.com>
2018-10-30 11:12:45 +0000

    Modify filtering behaviour based on node_env, prioritise error logs

--- index.js
+++ index.js
@@ -2,43 +2,41 @@ const { normalize } = require('path');
 
 module.exports = {
     log(msg) {
-        if (skip()) return;
-    
-        console.log(msg);
+        if (!skip()) console.log(msg);
     },
     info(msg) {
-        if (skip()) return;
-    
-        console.info(msg);
+        if (!skip()) console.info(msg);
     },
     error(msg) {
-        if (skip()) return;
-    
-        console.error(msg);
+        if (process.env.NODE_ENV == 'production' && !process.env.ENABLE_LOG) {
+            console.error(msg);
+        } else if (!skip()) {
+            console.error(msg);
+        }
     },
     warn(msg) {
-        if (skip()) return;
-    
-        console.warn(msg);
+        if (!skip()) console.warn(msg);
     },
     debug(msg) {
-        if (skip()) return;
-
-        console.debug(msg);
+        if (!skip()) console.debug(msg);
     }
 };
 
 function skip() {
-    if (!process.env.ENABLE_LOG) {
+    if (process.env.ENABLE_LOG) {
+        const loggables = process.env.ENABLE_LOG.split(';');
+
+        for (let i = 0; i < loggables.length; i++) {
+            if (module.parent.filename.indexOf(normalize(loggables[i])) != -1) {
+                return false;
+            }   
+        }
+
         return true;
     }
 
-    const loggables = process.env.ENABLE_LOG.split(';');
-
-    for (let i = 0; i < loggables.length; i++) {
-        if (module.parent.filename.indexOf(normalize(loggables[i])) != -1) {
-            return false;
-        }
+    if (process.env.NODE_ENV == 'development') {
+        return false;
     }
 
     return true;
