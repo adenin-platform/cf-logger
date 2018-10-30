c7705a9b22e48aa5f303f6783d9799178f429443
olliejm <olliejm@protonmail.com>
2018-10-30 14:30:31 +0000

    fix syntax

--- index.js
+++ index.js
@@ -3,23 +3,44 @@ const chalk = require('chalk');
 
 module.exports = {
     log(msg) {
-        if (!skip()) console.log(new Date().toISOString() + ': ' + chalk.green(msg));
+        if (!skip()) {
+            console.log(
+                new Date().toISOString() + ': ' + 
+                chalk.green(msg)
+            );
+        }
     },
     info(msg) {
-        if (!skip()) console.info(new Date().toISOString() + ': ' + chalk.green('INFO: ' + msg));
+        if (!skip()) {
+            console.info(
+                new Date().toISOString() + ': ' + 
+                chalk.green('INFO: ' + msg)
+            );
+        }
     },
     error(msg) {
-        if (!process.env.LOG_FILTER) {
-            console.error(new Date().toISOString() + ': ' + chalk.red('ERROR: ' + msg));
-        } else if (!skip()) {
-            console.error(new Date().toISOString() + ': ' + chalk.red('ERROR: ' + msg));
+        if (!process.env.LOG_FILTER || !skip()) {
+            console.error(
+                new Date().toISOString() + ': ' + 
+                chalk.red('ERROR: ' + msg)
+            );
         }
     },
     warn(msg) {
-        if (!skip()) console.warn(new Date().toISOString() + ': ' + chalk.yellow('WARN: ' + msg));
+        if (!skip()) {
+            console.warn(
+                new Date().toISOString() + ': ' + 
+                chalk.yellow('WARN: ' + msg)
+            );
+        }
     },
     debug(msg) {
-        if (!skip()) console.debug(new Date().toISOString() + ': ' + chalk.magenta('DEBUG: ' + msg));
+        if (!skip()) {
+            console.debug(
+                new Date().toISOString() + ': ' + 
+                chalk.magenta('DEBUG: ' + msg)
+            );
+        }
     }
 };
 
@@ -28,7 +49,9 @@ function skip() {
         const loggables = process.env.LOG_FILTER.split(';');
 
         for (let i = 0; i < loggables.length; i++) {
-            if (module.parent.filename.indexOf(normalize(loggables[i])) != -1) {
+            if (module.parent.filename.indexOf(
+                    normalize(loggables[i])
+                ) != -1) {
                 return false;
             }   
         }
