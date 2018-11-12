06a58b22ee6f216ab36a198443975cdb47db77e5
olliejm <olliejm@protonmail.com>
2018-11-12 11:56:43 +0000

    prettier

--- index.js
+++ index.js
@@ -6,37 +6,27 @@ const date = () => chalk.grey(new Date().toISOString() + ': ');
 module.exports = {
     log(msg) {
         if (!skip()) {
-            console.log(
-                date() + chalk.blue(chalk.bold('LOG:   ') + msg)
-            );
+            console.log(date() + chalk.blue(chalk.bold('LOG:   ') + msg));
         }
     },
     info(msg) {
         if (!skip()) {
-            console.info(
-                date() + chalk.green(chalk.bold('INFO:  ') + msg)
-            );
+            console.info(date() + chalk.green(chalk.bold('INFO:  ') + msg));
         }
     },
     error(msg) {
         if (!process.env.LOG_FILTER || !skip()) {
-            console.error(
-                date() + chalk.red(chalk.bold('ERROR: ') + msg)
-            );
+            console.error(date() + chalk.red(chalk.bold('ERROR: ') + msg));
         }
     },
     warn(msg) {
         if (!skip()) {
-            console.warn(
-                date() + chalk.yellow(chalk.bold('WARN:  ') + msg)
-            );
+            console.warn(date() + chalk.yellow(chalk.bold('WARN:  ') + msg));
         }
     },
     debug(msg) {
         if (!skip()) {
-            console.debug(
-                date() + chalk.magenta(chalk.bold('DEBUG: ') + msg)
-            );
+            console.debug(date() + chalk.magenta(chalk.bold('DEBUG: ') + msg));
         }
     }
 };
@@ -46,11 +36,9 @@ function skip() {
         const loggables = process.env.LOG_FILTER.split(';');
 
         for (let i = 0; i < loggables.length; i++) {
-            if (module.parent.filename.indexOf(
-                normalize(loggables[i])
-            ) != -1) {
+            if (module.parent.filename.indexOf(normalize(loggables[i])) != -1) {
                 return false;
-            }   
+            }
         }
 
         return true;
