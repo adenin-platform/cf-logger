4a3477d1b23179f42e94e26a4b30413040da9e9a
olliejm <olliejm@protonmail.com>
2019-02-04 12:05:29 +0000

    handle logging of objects

--- index.js
+++ index.js
@@ -9,27 +9,52 @@ const date = () => chalk.grey(new Date().toISOString() + ': ');
 module.exports = {
     log(msg) {
         if (!skip()) {
-            console.log(date() + chalk.blue(chalk.bold('LOG: ') + msg));
+            if (typeof msg === 'object') {
+                console.log(date() + chalk.blue.bold('LOG:'));
+                console.log(msg);
+            } else {
+                console.log(date() + chalk.blue.bold('LOG: ') + msg);
+            }
         }
     },
     info(msg) {
         if (!skip()) {
-            console.info(date() + chalk.green(chalk.bold('INFO: ') + msg));
+            if (typeof msg === 'object') {
+                console.info(date() + chalk.green.bold('INFO:'));
+                console.info(msg);
+            } else {
+                console.info(date() + chalk.green.bold('INFO: ') + msg);
+            }
         }
     },
     error(msg) {
         if (!process.env.LOG_FILTER || !skip()) {
-            console.error(date() + chalk.red(chalk.bold('ERROR: ') + msg));
+            if (typeof msg === 'object') {
+                console.error(date() + chalk.red.bold('ERROR:'));
+                console.error(msg);
+            } else {
+                console.error(date() + chalk.red.bold('ERROR: ') + msg);
+            }
         }
     },
     warn(msg) {
         if (!skip()) {
-            console.warn(date() + chalk.yellow(chalk.bold('WARN: ') + msg));
+            if (typeof msg === 'object') {
+                console.warn(date() + chalk.yellow.bold('WARN:'));
+                console.warn(msg);
+            } else {
+                console.warn(date() + chalk.yellow.bold('WARN: ') + msg);
+            }
         }
     },
     debug(msg) {
         if (!skip()) {
-            console.debug(date() + chalk.magenta(chalk.bold('DEBUG: ') + msg));
+            if (typeof msg === 'object') {
+                console.debug(date() + chalk.magenta.bold('DEBUG:'));
+                console.debug(msg);
+            } else {
+                console.debug(date() + chalk.magenta.bold('DEBUG: ') + msg);
+            }
         }
     }
 };
