01e2d16d227aa4bcee907b82cd05d60575981ce5
olliejm <olliejm@protonmail.com>
2019-03-25 12:48:53 +0000

    use arrow functions

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ const chalk = require('chalk');
 const date = () => chalk.grey(new Date().toISOString() + ':');
 
 module.exports = {
-  log(...args) {
+  log: (...args) => {
     if (process.env.NODE_ENV === 'development') console.log(...[date(), chalk.blue.bold('LOG:')].concat(args));
   },
-  info(...args) {
+  info: (...args) => {
     if (process.env.NODE_ENV === 'development') console.info(...[date(), chalk.green.bold('INFO:')].concat(args));
   },
-  error(...args) {
+  error: (...args) => {
     console.error(...[date(), chalk.red.bold('ERROR:')].concat(args));
   },
-  warn(...args) {
+  warn: (...args) => {
     if (process.env.NODE_ENV === 'development') console.warn(...[date(), chalk.yellow.bold('WARN:')].concat(args));
   },
-  debug(...args) {
+  debug: (...args) => {
     if (process.env.NODE_ENV === 'development') console.debug(...[date(), chalk.magenta.bold('DEBUG:')].concat(args));
   }
 };
