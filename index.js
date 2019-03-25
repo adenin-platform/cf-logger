44387b7ae57994a6158ec7a0842a938d424fdefd
olliejm <olliejm@protonmail.com>
2019-03-25 10:37:20 +0000

    Remove LOG_FILTER

--- index.js
+++ index.js
@@ -1,55 +1,24 @@
 /* eslint-disable no-console */
 'use strict';
 
-const {normalize} = require('path');
 const chalk = require('chalk');
 
 const date = () => chalk.grey(new Date().toISOString() + ':');
 
 module.exports = {
   log(...args) {
-    if (!skip()) {
-      console.log(...[date(), chalk.blue.bold('LOG:')].concat(args));
-    }
+    if (process.env.NODE_ENV === 'development') console.log(...[date(), chalk.blue.bold('LOG:')].concat(args));
   },
   info(...args) {
-    if (!skip()) {
-      console.info(...[date(), chalk.green.bold('INFO:')].concat(args));
-    }
+    if (process.env.NODE_ENV === 'development') console.info(...[date(), chalk.green.bold('INFO:')].concat(args));
   },
   error(...args) {
-    if (!process.env.LOG_FILTER || !skip()) {
-      console.error(...[date(), chalk.red.bold('ERROR:')].concat(args));
-    }
+    console.error(...[date(), chalk.red.bold('ERROR:')].concat(args));
   },
   warn(...args) {
-    if (!skip()) {
-      console.warn(...[date(), chalk.yellow.bold('WARN:')].concat(args));
-    }
+    if (process.env.NODE_ENV === 'development') console.warn(...[date(), chalk.yellow.bold('WARN:')].concat(args));
   },
   debug(...args) {
-    if (!skip()) {
-      console.debug(...[date(), chalk.magenta.bold('DEBUG:')].concat(args));
-    }
+    if (process.env.NODE_ENV === 'development') console.debug(...[date(), chalk.magenta.bold('DEBUG:')].concat(args));
   }
 };
-
-function skip() {
-  if (process.env.LOG_FILTER) {
-    const loggables = process.env.LOG_FILTER.split(';');
-
-    for (let i = 0; i < loggables.length; i++) {
-      if (module.parent.filename.indexOf(normalize(loggables[i])) !== -1) {
-        return false;
-      }
-    }
-
-    return true;
-  }
-
-  if (process.env.NODE_ENV === 'development') {
-    return false;
-  }
-
-  return true;
-}
