5df06cfd20fd9bb37b0954df7f427be79f434c3a
olliejm <olliejm@protonmail.com>
2019-09-10 12:52:57 +0100

    working cloud log implementation (await-free promise rejection handling)

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 /* eslint-disable no-console */
 'use strict';
 
-const fs = require('fs');
-const {promisify} = require('util');
-
-const readFile = promisify(fs.readFile);
-
 const chalk = require('chalk');
 const got = require('got');
-const yaml = require('js-yaml');
 
 const date = () => chalk.grey(new Date().toISOString() + ':');
 
@@ -19,34 +13,9 @@ module.exports = {
   info: (...args) => {
     if (process.env.NODE_ENV === 'development') console.info(...[date(), chalk.green.bold('INFO:')].concat(args));
   },
-  error: async (...args) => {
+  error: (...args) => {
     console.error(...[date(), chalk.red.bold('ERROR:')].concat(args));
-
-    if (process.env.LOG_SERVER_URL) {
-      const payload = {};
-
-      for (let i = 0; i < args.length; i++) {
-        if (args[i] instanceof String && !payload.message) {
-          payload.message = args[i];
-          continue;
-        }
-
-        if (args[i] instanceof Error) {
-          payload.message = args[i].message;
-          payload.stack = args[i].stack;
-        }
-      }
-
-      const definition = yaml.safeLoad(await readFile('./_definition.yaml'));
-
-      payload.connector = definition.ConnectorTitle;
-
-      got.post(process.env.LOG_SERVER_URL, {
-        method: 'POST',
-        json: true,
-        body: payload
-      });
-    }
+    sendCloudLog(args);
   },
   warn: (...args) => {
     if (process.env.NODE_ENV === 'development') console.warn(...[date(), chalk.yellow.bold('WARN:')].concat(args));
@@ -55,3 +24,35 @@ module.exports = {
     if (process.env.NODE_ENV === 'development') console.debug(...[date(), chalk.magenta.bold('DEBUG:')].concat(args));
   }
 };
+
+function sendCloudLog(args) {
+  if (!process.env.LOG_SERVER_URL) return;
+
+  const payload = {
+    host: process.env.HOST
+  };
+
+  for (let i = 0; i < args.length; i++) {
+    if (typeof args[i] === 'string' && !payload.message) {
+      payload.message = args[i];
+      continue;
+    }
+
+    if (args[i] instanceof Error) {
+      payload.message = args[i].message;
+      payload.stack = args[i].stack;
+      break;
+    }
+  }
+
+  const promise = got.post(process.env.LOG_SERVER_URL, {
+    method: 'POST',
+    json: true,
+    body: payload
+  });
+
+  promise.catch((error) => {
+    console.error(date(), chalk.red.bold('ERROR: when sending logs to cloud'), error);
+    promise.cancel();
+  });
+}
