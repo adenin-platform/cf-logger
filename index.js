4bd0757ec574bd478656923d3bb41bb1a26a705b
olliejm <olliejm@protonmail.com>
2019-09-10 10:16:37 +0100

    further modifications to cloud logging prototype

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 /* eslint-disable no-console */
 'use strict';
 
+const fs = require('fs');
+const {promisify} = require('util');
+
+const readFile = promisify(fs.readFile);
+
 const chalk = require('chalk');
 const got = require('got');
+const yaml = require('js-yaml');
 
 const date = () => chalk.grey(new Date().toISOString() + ':');
 
@@ -13,16 +19,32 @@ module.exports = {
   info: (...args) => {
     if (process.env.NODE_ENV === 'development') console.info(...[date(), chalk.green.bold('INFO:')].concat(args));
   },
-  error: (...args) => {
+  error: async (...args) => {
     console.error(...[date(), chalk.red.bold('ERROR:')].concat(args));
 
     if (process.env.LOG_SERVER_URL) {
+      const payload = {};
+
+      for (let i = 0; i < args.length; i++) {
+        if (args[i] instanceof String && !payload.message) {
+          payload.message = args[i];
+          continue;
+        }
+
+        if (args[i] instanceof Error) {
+          payload.message = args[i].message;
+          payload.stack = args[i].stack;
+        }
+      }
+
+      const definition = yaml.safeLoad(await readFile('./_definition.yaml'));
+
+      payload.connector = definition.ConnectorTitle;
+
       got.post(process.env.LOG_SERVER_URL, {
         method: 'POST',
         json: true,
-        body: {
-          error: args
-        }
+        body: payload
       });
     }
   },
